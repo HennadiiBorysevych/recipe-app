6f5ff2eeba2d22f44f362ee8815892ed7b952ab9
Hennadii Borysevych <genuch3@gmail.com>
2024-01-08 17:03:44 +0100

    feat:user tests

--- app/app/settings.py
+++ app/app/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'core',
     'user',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
     'django.contrib.admin',
     'django.contrib.auth',
